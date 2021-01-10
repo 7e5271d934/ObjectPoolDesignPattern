0607523cd7f9d317af71f29d3dc72b98e9fe5e1a
7e5271d934 <HBuqLEP2YebfD0ZAr1of@protonmail.com>
2021-01-10 14:30:17 -0100

    Created getCar() method on ObjectPool class

--- src/com/ratchet/ObjectPool.java
+++ src/com/ratchet/ObjectPool.java
@@ -1,10 +1,21 @@
 package com.ratchet;
 
+import com.ratchet.reusables.Car;
+import com.ratchet.reusables.Reusable;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Random;
+
 public class ObjectPool {
 
     private static ObjectPool objectPoolInstance;
 
+    private Object[] reusableObjects;
+    private static final int MAX_OBJECTS = 100;
+
     private ObjectPool() {
+        reusableObjects = new Object[MAX_OBJECTS];
         objectPoolInstance = this;
     }
 
@@ -17,7 +28,19 @@ public class ObjectPool {
     }
 
     public void init() {
+        for (int i = 0; i < reusableObjects.length; i++) {
+            reusableObjects[i] = new Car("Toyota " + i);
+        }
+    }
 
+    public Object getCar(String brand) {
+        Car car = null;
+        for (Object obj: reusableObjects) {
+           if (obj.toString().equals(brand.toLowerCase())) {
+               car = (Car) obj;
+           }
+        }
+        return car;
     }
 
 }
