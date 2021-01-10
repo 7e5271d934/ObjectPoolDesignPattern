f6520226332b9d9dafe145305dd4832ccf9d363e
7e5271d934 <HBuqLEP2YebfD0ZAr1of@protonmail.com>
2021-01-10 13:47:45 -0100

    Created Car class

---
+++ src/com/ratchet/reusables/Car.java
@@ -0,0 +1,14 @@
+package com.ratchet.reusables;
+
+public class Car {
+
+    private String brand;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+}
