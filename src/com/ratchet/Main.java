79559e8a6674e12ad1289b95c1dabd5f49a93419
7e5271d934 <HBuqLEP2YebfD0ZAr1of@protonmail.com>
2021-01-18 19:45:08 -0100

    Implemented getObj() method

--- src/com/ratchet/Main.java
+++ src/com/ratchet/Main.java
@@ -1,13 +1,17 @@
 package com.ratchet;
 
+import com.ratchet.reusables.Car;
 import com.ratchet.reusables.Reusables;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(ObjectPool.getInstance().getCar("brand 25"));
-        ObjectPool.getInstance().dispose("brand 25", Reusables.CAR);
-        System.out.println(ObjectPool.getInstance().getCar("brand 25"));
+        // ObjectPool.getInstance().dispose("brand 25", Reusables.CAR);
+        // System.out.println(ObjectPool.getInstance().getCar("brand 25"));
+
+        Car car = (Car) ObjectPool.getInstance().getObj("brand 25", Reusables.CAR);
+        System.out.println(car.getBrand());
 
     }
 }
